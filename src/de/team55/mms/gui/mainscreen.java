a5b7743a8706ee0dac371e051620a0faad26b604
Christoph <CBlackJoker@gmx.de>
2013-09-18 09:26:03 +0200

    fach

--- src/de/team55/mms/gui/mainscreen.java
+++ src/de/team55/mms/gui/mainscreen.java
@@ -895,15 +895,14 @@ public class mainscreen {
 		
 		
 		JPanel pnl_fach = new JPanel();
-		tabbedPane.addTab("Standard Felder", null, pnl_fach, "Standard Felder von Modulen verwalten");
+		tabbedPane.addTab("Fächer", null, pnl_fach, "Fächer verwalten");
 		pnl_felder.setLayout(new BorderLayout(0, 0));
 
 //		JScrollPane scrollPane_1 = new JScrollPane();
-//		pnl_felder.add(scrollPane_1, BorderLayout.CENTER);
+//		pnl_fach.add(scrollPane_1, BorderLayout.CENTER);
 		JPanel pnl_fach_content = new JPanel();
 		JPanel pnl_fach_btns = new JPanel();
 		
-		//angefangen fach hinzuzufügen model muss woanderst gefüllt werden/serverconnection wonaderst 
 		fachzws = new ArrayList<Fach>();
 		
 		JList<Fach> fachlist = new JList<Fach>(fachmodel);
